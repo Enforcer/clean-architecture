47c5de57fd87bb7a3e9ae6449c16dce553925c5f
Sebastian Buczyński <nnplaya@gmail.com>
2019-01-25 20:23:24 +0100

    Add abstract queries

---
+++ auctions/auctions/auctions/application/queries/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    'GetActiveAuctions',
+    'GetSingleAuction',
+]
+
+from auctions.application.queries.auctions import (
+    GetActiveAuctions,
+    GetSingleAuction,
+)
---
+++ auctions/auctions/auctions/application/queries/auctions.py
@@ -0,0 +1,27 @@
+import abc
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+
+from auctions.domain.value_objects import Money
+
+
+@dataclass
+class AuctionDto:
+    id: int
+    title: str
+    current_price: Money
+    starting_price: Money
+    ends_at: datetime
+
+
+class GetSingleAuction(abc.ABC):
+    @abc.abstractmethod
+    def query(self, auction_id: int) -> AuctionDto:
+        pass
+
+
+class GetActiveAuctions(abc.ABC):
+    @abc.abstractmethod
+    def query(self) -> List[AuctionDto]:
+        pass
