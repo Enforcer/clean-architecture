4c00a9191648abc638c776ce5dcb28835e7fee5f
Sebastian Buczyński <nnplaya@gmail.com>
2019-05-19 17:24:29 +0200

    Tie payments facade with concrete connection

--- auctions/payments/payments/__init__.py
+++ auctions/payments/payments/__init__.py
@@ -27,38 +27,34 @@ def run_charge_in_background(event: PaymentCharged) -> None:
 class PaymentsFacade:
     def __init__(self, config: PaymentsConfig, conn_provider: Callable[[], Connection], event_bus: EventBus) -> None:
         self._api_consumer = ApiConsumer(config.username, config.password)
-        self._conn_provider = conn_provider
+        self._connection = conn_provider()
         self._event_bus = event_bus
 
     def get_pending_payments(self, customer_id: int) -> List[dao.PaymentDto]:
-        return dao.get_pending_payments(customer_id, self._conn_provider())
+        return dao.get_pending_payments(customer_id, self._connection)
 
     def start_new_payment(self, payment_uuid: UUID, customer_id: int, amount: Money, description: str) -> None:
-        dao.start_new_payment(payment_uuid, customer_id, amount, description, self._conn_provider())
+        dao.start_new_payment(payment_uuid, customer_id, amount, description, self._connection)
         self._event_bus.post(PaymentStarted(payment_uuid))
 
     def pay(self, payment_uuid: UUID, customer_id: int, token: str) -> None:
-        payment = dao.get_payment(payment_uuid, customer_id, self._conn_provider())
+        payment = dao.get_payment(payment_uuid, customer_id, self._connection)
         if payment.status != dao.PaymentStatus.NEW.value:
             raise Exception(f"Can't pay - unexpected status {payment.status}")
 
         try:
             charge_id = self._api_consumer.charge(payment.amount, token)
         except PaymentFailedError:
-            dao.update_payment(
-                payment_uuid, customer_id, {"status": dao.PaymentStatus.FAILED.value}, self._conn_provider()
-            )
+            dao.update_payment(payment_uuid, customer_id, {"status": dao.PaymentStatus.FAILED.value}, self._connection)
             self._event_bus.post(PaymentFailed(payment_uuid))
         else:
             update_values = {"status": dao.PaymentStatus.CHARGED.value, "charge_id": charge_id}
-            dao.update_payment(payment_uuid, customer_id, update_values, self._conn_provider())
+            dao.update_payment(payment_uuid, customer_id, update_values, self._connection)
             self._event_bus.post(PaymentCharged(payment_uuid))
 
     def capture_payment(self, payment_uuid: UUID, customer_id: int) -> None:
-        charge_id = dao.get_payment_charge_id(payment_uuid, customer_id, self._conn_provider())
+        charge_id = dao.get_payment_charge_id(payment_uuid, customer_id, self._connection)
         assert charge_id, f"No charge_id available for {payment_uuid}, aborting capture"
         self._api_consumer.capture(charge_id)
-        dao.update_payment(
-            payment_uuid, customer_id, {"status": dao.PaymentStatus.CAPTURED.value}, self._conn_provider()
-        )
+        dao.update_payment(payment_uuid, customer_id, {"status": dao.PaymentStatus.CAPTURED.value}, self._connection)
         self._event_bus.post(PaymentCaptured(payment_uuid))
