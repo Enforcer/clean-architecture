a325b306e77747438723685b0ae7329b17b97623
Sebastian Buczyński <nnplaya@gmail.com>
2019-05-23 09:57:34 +0200

    Write Auctions module

---
+++ auctions/auctions/auctions/__init__.py
@@ -0,0 +1,15 @@
+import injector
+
+from auctions.application.repositories import AuctionsRepository
+from auctions.application.use_cases.placing_bid import PlacingBid, PlacingBidOutputBoundary
+from auctions.application.use_cases.withdrawing_bids import WithdrawingBids
+
+
+class Auctions(injector.Module):
+    @injector.provider
+    def placing_bid_uc(self, boundary: PlacingBidOutputBoundary, repo: AuctionsRepository) -> PlacingBid:
+        return PlacingBid(boundary, repo)
+
+    @injector.provider
+    def withdrawing_bids_uc(self, repo: AuctionsRepository) -> WithdrawingBids:
+        return WithdrawingBids(repo)
