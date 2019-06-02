86e72c6dce33f1d7a44f80b20186c67cfe32dc80
Sebastian Buczyński <nnplaya@gmail.com>
2019-06-03 00:04:07 +0200

    Add backend-agnostic GUID type (from SA docs)

--- auctions/db_infrastructure/db_infrastructure/__init__.py
+++ auctions/db_infrastructure/db_infrastructure/__init__.py
@@ -1,5 +1,47 @@
+import uuid
+
 from sqlalchemy import MetaData
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.types import CHAR, TypeDecorator
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
+
+
+class GUID(TypeDecorator):
+    """Platform-independent GUID type.
+
+    Uses PostgreSQL's UUID type, otherwise uses
+    CHAR(32), storing as stringified hex values.
+
+    Source: https://docs.sqlalchemy.org/en/13/core/custom_types.html#backend-agnostic-guid-type
+    """
+
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == "postgresql":
+            return dialect.type_descriptor(UUID())
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == "postgresql":
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return "%.32x" % uuid.UUID(value).int
+            else:
+                # hexstring
+                return "%.32x" % value.int
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, uuid.UUID):
+                value = uuid.UUID(value)
+            return value
