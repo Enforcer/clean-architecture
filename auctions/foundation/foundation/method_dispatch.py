220673affd6ca6c29250625f280b4302795927cf
Sebastian Buczyński <nnplaya@gmail.com>
2019-06-03 00:02:23 +0200

    Pass registry in method_dispatch

--- auctions/foundation/foundation/method_dispatch.py
+++ auctions/foundation/foundation/method_dispatch.py
@@ -9,5 +9,6 @@ def method_dispatch(func: Callable[..., Any]) -> Callable[..., Any]:
         return dispatcher.dispatch(args[1].__class__)(*args, **kw)
 
     wrapper.register = dispatcher.register  # type: ignore
+    wrapper.registry = dispatcher.registry  # type: ignore
     functools.update_wrapper(wrapper, func)
     return wrapper
