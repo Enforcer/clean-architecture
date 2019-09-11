41bce70467fdb29fee5ceb4f209a28f571a9f63f
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-09-11 10:38:15 +0200

    Add implementation of ship_package endpoint based on the use case

--- auctioning_platform/web_app/web_app/blueprints/shipping.py
+++ auctioning_platform/web_app/web_app/blueprints/shipping.py
@@ -1,7 +1,10 @@
+# import uuid
+
 from flask import Blueprint, Response, abort, jsonify, make_response
 from flask_login import current_user
 
-from shipping import GetNextPackage  # ShippingPackage, ShippingPackageInputDto
+from shipping import GetNextPackage, ShippingPackage  # ShippingPackageInputDto
+# from shipping.domain.exceptions import PackageAlreadyShipped
 
 shipping_blueprint = Blueprint("shipping_blueprint", __name__)
 
@@ -11,9 +14,13 @@ def get_next_package(query: GetNextPackage) -> Response:
     return make_response(jsonify(query.query()))
 
 
-@shipping_blueprint.route("/package/<package_uuid>/send", methods=["POST"])
-def send_package(package_uuid: str) -> Response:
+@shipping_blueprint.route("/package/<package_uuid>/ship", methods=["POST"])
+def ship_package(package_uuid: str, shipping_package_uc: ShippingPackage) -> Response:
     if not current_user.is_authenticated:
         abort(403)
-
+    # try:
+    #     shipping_package_uc.execute(ShippingPackageInputDto(uuid.UUID(package_uuid)))
+    # except PackageAlreadyShipped:
+    #     abort(make_response(jsonify({"message": f"Package '{package_uuid}' has been shipped already"}), 400))
+    # return make_response(jsonify({"message": f"Package '{package_uuid}' shipped successfully"}))
     return make_response()
