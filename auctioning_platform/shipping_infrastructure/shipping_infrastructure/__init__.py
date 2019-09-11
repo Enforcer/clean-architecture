a0a71f5bf39d459b4b6f74e0291f900e9119915b
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-09-11 09:34:50 +0200

    Put the GetNextPackage query into dependency injection | commented out

--- auctioning_platform/shipping_infrastructure/shipping_infrastructure/__init__.py
+++ auctioning_platform/shipping_infrastructure/shipping_infrastructure/__init__.py
@@ -1,8 +1,10 @@
 import injector
+# from sqlalchemy.engine import Connection
 
-from shipping import AddressRepository
+from shipping import AddressRepository  # GetNextPackage
 
 from shipping_infrastructure.models import packages
+# from shipping_infrastructure.queries import SqlGetNextPackage
 from shipping_infrastructure.repositories import FakeAddressRepository
 
 __all__ = [
@@ -14,6 +16,10 @@ __all__ = [
 
 
 class ShippingInfrastructure(injector.Module):
+    # @injector.provider
+    # def get_next_package_query(self, conn: Connection) -> GetNextPackage:
+    #     return SqlGetNextPackage(conn)
+
     @injector.provider
     def address_repo(self) -> AddressRepository:
         return FakeAddressRepository()
