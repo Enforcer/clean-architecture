1aa01bc6fe55c1d463b39410825f823b6541b80e
Sebastian Buczyński <nnplaya@gmail.com>
2020-10-16 22:38:49 +0200

    Update Flask-Login

--- auctioning_platform/web_app/web_app/tests/test_endpoints.py
+++ auctioning_platform/web_app/web_app/tests/test_endpoints.py
@@ -97,16 +97,16 @@ def another_user(connection: Connection) -> Generator[str, None, None]:
 
 @pytest.fixture()
 def logged_in_user(create_remove_user: str, client: testing.FlaskClient) -> None:
-    with client.session_transaction() as session:  # type: ignore
-        session["user_id"] = create_remove_user
-        session["_fresh"] = True
+    client.post(
+        "/login",
+        json={"email": "test+login@cleanarchitecture.io", "password": "Dummy123!"},
+    )
 
 
 @pytest.mark.usefixtures("remove_user")
 def test_register(client: testing.FlaskClient, connection: Connection) -> None:
     response = client.post(
         "/register",
-        headers={"Content-type": "application/json"},
         json={"email": "test+register@cleanarchitecture.io", "password": "Dummy123!"},
     )
 
@@ -125,14 +125,16 @@ def assert_customer_with_given_email_exists(connection: Connection, email: str) 
 def test_login(client: testing.FlaskClient, create_remove_user: str) -> None:
     response = client.post(
         "/login",
-        headers={"Content-type": "application/json"},
         json={"email": "test+login@cleanarchitecture.io", "password": "Dummy123!"},
     )
 
     assert response.status_code == 200
     json_response_body = response.json.copy()
     json_response_body["response"]["user"].pop("authentication_token")
-    assert json_response_body == {"meta": {"code": 200}, "response": {"user": {"id": create_remove_user}}}
+    assert json_response_body == {
+        "meta": {"code": 200},
+        "response": {"user": {"id": create_remove_user}},
+    }
 
 
 @pytest.fixture()
@@ -140,7 +142,12 @@ def example_auction(connection: Connection, another_user: str) -> Generator[int,
     ends_at = datetime.now() + timedelta(days=3)
     result_proxy = connection.execute(
         auctions.insert(
-            {"title": "Super aukcja", "starting_price": "0.99", "current_price": "1.00", "ends_at": ends_at}
+            {
+                "title": "Super aukcja",
+                "starting_price": "0.99",
+                "current_price": "1.00",
+                "ends_at": ends_at,
+            }
         )
     )
     auction_id = result_proxy.lastrowid
@@ -166,9 +173,7 @@ def test_returns_list_of_auctions(client: testing.FlaskClient) -> None:
 
 @pytest.mark.usefixtures("logged_in_user")
 def test_places_bid(client: testing.FlaskClient, example_auction: int) -> None:
-    response = client.post(
-        f"/auctions/{example_auction}/bids", headers={"Content-type": "application/json"}, json={"amount": "15.99"}
-    )
+    response = client.post(f"/auctions/{example_auction}/bids", json={"amount": "15.99"})
 
     assert response.status_code == 200
     assert response.json == {"message": "Hooray! You are a winner"}
